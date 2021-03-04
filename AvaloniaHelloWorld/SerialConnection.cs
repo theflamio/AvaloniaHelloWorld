659ff560a1487c210a20012caab393bc7ed02c86
Flemming Christensen <flamekfor@gmail.com>
2021-03-04 22:50:18 +0100

    added Serial and Sqlite class
    Made the very BASIC UI

---
+++ AvaloniaHelloWorld/SerialConnection.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.IO.Ports;
+
+
+namespace EC_test_app2
+{
+    enum SerialConnectionStatus
+    {
+        Disconnected,
+        Connected,
+        Error,
+        Timeout
+    }
+    class SerialConnection
+    {     
+        public string connectedPort = null;
+        public string connectedDevice = null;
+
+        private static SerialPort _serialPort = new SerialPort();
+        private string[] portnames;
+        private string message = null;
+        private SerialConnectionStatus status = SerialConnectionStatus.Disconnected;
+
+        public string[] SearchForConnectedDevices()
+        {
+            portnames = SerialPort.GetPortNames();
+            return portnames;
+        }
+
+        public SerialConnectionStatus ConnectToSerial(string portName)
+        {
+            if (status == SerialConnectionStatus.Disconnected)
+            {
+                _serialPort.PortName = portName;
+                _serialPort.BaudRate = 115200;
+                _serialPort.ReadTimeout = 1500;
+                _serialPort.Open();
+                try
+                {
+                    _serialPort.DiscardInBuffer();
+                    message = _serialPort.ReadLine();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                if (message != null)
+                {
+                    if (message.Contains("EC-Test SN:"))
+                    {
+                        _serialPort.Write("x");
+                        status = SerialConnectionStatus.Connected;
+                        connectedPort = portName;
+                        connectedDevice = message;
+                        return SerialConnectionStatus.Connected;
+                    }                  
+                }
+                _serialPort.Close();
+                return SerialConnectionStatus.Error;
+            }
+            else
+            {
+                return SerialConnectionStatus.Error;
+            }
+        }
+
+        public string SerialMessageReceiver()
+        {            
+                string buf = _serialPort.ReadLine();
+                string channel = buf.Substring(0, buf.IndexOf("="));
+                string value = buf.Substring(3, buf.IndexOf("\r") - 3);
+                                
+                return value + "\n";            
+        }
+    
+        public string SerialMessageTransmitter()
+        {
+            //TODO make a serialWriter
+            return "It went fine";
+        }       
+
+        public SerialConnectionStatus DisconnectFromSerial()
+        {
+            if (_serialPort.IsOpen)
+            {
+                _serialPort.Write("q");
+                status = SerialConnectionStatus.Disconnected;
+                connectedPort = null;
+                connectedDevice = null;
+                _serialPort.Close();
+                return status;
+            }
+            return SerialConnectionStatus.Error;
+        }
+
+        public string getConnectedDevice()
+        {
+            return connectedDevice;
+        }
+
+        public SerialConnectionStatus getConnectionsStatus() 
+        {
+            return status;
+        }
+
+        ~SerialConnection()
+        {
+            if (_serialPort.IsOpen)
+            {
+                _serialPort.Close();
+                _serialPort.Dispose();
+            }
+        }
+    }
+}
---
+++ AvaloniaHelloWorld/Sqlite.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EC_test_app2
+{
+    class Sqlite
+    {
+        public void ConnectToSqLite() { }
+
+        public void DisconnectToSqLite() { }
+
+        public void LogDataSqLite() { }
+
+        public void FecthDataSqLite() { }
+    }
+}
